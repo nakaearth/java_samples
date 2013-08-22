a07cd080e9407c8570491beba7750095141a22cc
nakamura shinichirou <naka5313@gmail.com>
2013-08-22 14:26:14 +0900

    例外処理についてのサンプルコード

---
+++ src/sample5/TestExceptionDemo.java
@@ -0,0 +1,19 @@
+package sample5;
+
+import java.io.IOException;
+
+public class TestExceptionDemo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		TestFileUtil util = new TestFileUtil();
+		try {
+			util.getData();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		util.getData2();
+	}
+}
