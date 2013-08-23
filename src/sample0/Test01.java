0d81988a2b9d8a1704cbb5acbeb7dbe799de6269
nakamura shinichirou <naka5313@gmail.com>
2013-08-23 15:04:18 +0900

    サンプルを追加。

---
+++ src/sample0/Test01.java
@@ -0,0 +1,13 @@
+package sample0;
+
+public class Test01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("test hello");
+		System.out.println(args[0] + "," + args[1]);
+	}
+
+}
