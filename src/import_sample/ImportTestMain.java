133d4c55eabdce805a1e085ddbda92dc502792f3
nakamura shinichirou <naka5313@gmail.com>
2013-09-30 14:37:03 +0900

    importのテスト追加

---
+++ src/import_sample/ImportTestMain.java
@@ -0,0 +1,13 @@
+package import_sample;
+
+import static java.util.Locale.JAPAN;
+
+public class ImportTestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(JAPAN.getLanguage());
+	}
+}
