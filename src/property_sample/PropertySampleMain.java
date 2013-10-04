54b7a6907682910ba11d08624abec162705da7cc
nakamura shinichirou <naka5313@gmail.com>
2013-10-04 17:59:58 +0900

    プロパティの読み込みサンプル

---
+++ src/property_sample/PropertySampleMain.java
@@ -0,0 +1,31 @@
+package property_sample;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Properties;
+import java.util.ResourceBundle;
+
+
+public class PropertySampleMain {
+
+	public static void main(String[] args) {
+		Properties conf = new Properties();
+		try {
+			conf.load(PropertySampleMain.class.getResourceAsStream("/test.properties"));
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		System.out.println("name:" + conf.getProperty("name"));
+		System.out.println("db:" + conf.getProperty("db"));
+		System.out.println("url:" + conf.getProperty("url"));
+
+		System.out.println("=========================");
+		ResourceBundle rb = ResourceBundle.getBundle("test");
+		for (Enumeration e = rb.getKeys(); e.hasMoreElements();) {
+			String key = (String) e.nextElement();
+			String val = rb.getString(key);
+			System.out.println(key + ":" + val);
+		}
+	}
+
+}
